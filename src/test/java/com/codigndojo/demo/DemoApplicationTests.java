8fbfb9f575606c50d460806b6814c89fa4933d2b
AbdullahKimrigh <abdullahkimregh@gmail.com>
2022-08-29 14:16:08 +0300

    add files

---
+++ src/test/java/com/codigndojo/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codigndojo.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
